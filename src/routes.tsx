1bad007f72a559820410a803b34410658f38bb08
JuanNaCl <jgarciaot@unal.edu.co>
2024-12-16 18:42:18 -0500

    ✨ feat: Listado de vehiculo, eliminar y mandar al form de editar

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import Personal from './pages/forms-pages/personal';
 import PersonalListPage from './pages/list-pages/personal-list';
 import VehiclePage from './pages/forms-pages/vehiculo';
+import VehiclesListPage from './pages/list-pages/vehiculo-list';
 
 const AppRoutes = () => (
   <Router>
@@ -11,6 +12,7 @@ const AppRoutes = () => (
       <Route path="/personal-forms" element={<Personal />} />
       <Route path="/personal-list" element={<PersonalListPage />} />
       <Route path="/vehiculo-forms" element={<VehiclePage />} />
+      <Route path="/vehiculo-list" element={<VehiclesListPage />} />
       {/* Add routes for other sections */}
     </Routes>
   </Router>
